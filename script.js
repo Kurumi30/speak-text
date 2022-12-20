5f743c3877529e9446721a1c7cf8764a284739e3
Kurumi30 <bsd3su@gmail.com>
2022-12-20 19:07:14 -0300

    Your branch is up to date with 'origin/main'.

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const button = document.getElementById("button")
 button.addEventListener('click', () => {
     const voice = speechSynthesis
     const talk = new SpeechSynthesisUtterance(text.value)
-
+    let language = "pt-BR" // Default language
+    
+    talk.lang = language
     voice.speak(talk)
 
     if(text.value === "") {
