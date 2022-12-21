bf82a7f64bc5af94587deb6e28a5368d617b53c3
Kurumi30 <bsd3su@gmail.com>
2022-12-21 16:48:07 -0300

    Your branch is up to date with 'origin/main'.

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const text = document.getElementById("text")
 const button = document.getElementById("button")
+const clear = document.getElementById("clear")
 
 button.addEventListener('click', (e) => {
     e.preventDefault()
-    
+
     const voice = speechSynthesis
     const talk = new SpeechSynthesisUtterance(text.value)
-    let language = "pt-BR" // Default language
+    let randomVoices = Math.floor(Math.random() * 10)
+    /*let language = "pt-BR" // Default language
 
-    talk.lang = language
+    talk.lang = language*/
+    talk.volume = 1
+    talk.voice = voice.getVoices()[randomVoices]
     voice.speak(talk)
 
-    if(text.value === "") {
+    if (text.value === "") {
         alert("Digite algo para falar!")
+    }
+})
+
+clear.addEventListener("click", () => {
+    if (text.value !== "") {
+        clear.removeAttribute("disabled", false)
+        text.value = ""
     }
 })
